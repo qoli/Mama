b605114a7f50a6821f523e2979bb76b26e0fb41a
Qoli Wong <llqoli@gmail.com>
2013-12-24 11:34:41 +0800

    1.1.4.1224

--- update.php
+++ update.php
@@ -1,19 +1,13 @@
 <? include("include_Top.php");?>
-
-
 <div id="about_app" class="row opa isAni">
 	<div class="col-md-6">
 		<div data-spy="affix" data-offset-top="40">
 			<img class="h278" src="assets/empty.png" /></p>
 		</div>
-
-	</div>		
-
+	</div>
 	<div class="col-md-6 text_left">
 		<? include("notes.html");?>
 	</div>
 </div>
-
-
 <? include("include_Footer.php");?>
 <script type="text/javascript">$('.update').addClass('active');</script>
